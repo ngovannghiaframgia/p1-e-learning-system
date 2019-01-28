3ee1cd18af77a47edaaee933842ae8f7873e1688
ngovannghia1997kma <ngovannghia1997kma@gmail.com>
2019-01-28 09:34:31 +0700

    Test-for-Video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ApplicationRecord
   belongs_to :subject
   belongs_to :user
-  has_many :lesson_videos, dependent: :destroy
+  has_many :lesson_videos, foreign_key: :lesson_id, dependent: :destroy
 end
