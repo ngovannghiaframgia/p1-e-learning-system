afad2cf346bb89448f0f3903e8b5998d43cd1861
ngovannghia1997kma <ngovannghia1997kma@gmail.com>
2019-01-28 09:34:31 +0700

    Test-for-Lesson

--- db/migrate/20190130061823_create_lesson_videos.rb
+++ db/migrate/20190130061823_create_lesson_videos.rb
@@ -2,9 +2,11 @@ class CreateLessonVideos < ActiveRecord::Migration[5.2]
   def change
     create_table :lesson_videos do |t|
       t.references :lesson, foreign_key: true
+      t.references :video, foreign_key: true
 
       t.timestamps
     end
     add_index :lesson_videos, [:lesson_id, :created_at]
+    add_index :lesson_videos, [:video_id, :created_at]
   end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Lesson, type: :model do
-  context "Course should have many Lesson" do
-    it {is_expected.to belong_to :course}
+  context "#Relationships" do
+    it { is_expected.to have_many :lesson_videos }
+    it { is_expected.to have_many :homeworks }
+    it { is_expected.to have_many :comment_lessons }
+    it { is_expected.to belong_to :course }
   end
 end
